d3c73ee2e4feb571c398085a0049ae0692a66de3
sora <60399731+6days9weeks@users.noreply.github.com>
2022-03-11 20:56:16 +0900

    Update __init__.py

--- mcparser/__init__.py
+++ mcparser/__init__.py
@@ -168,7 +168,7 @@ class McParser(commands.Cog):
             await ctx.send("Index cannot be 0.")
             return
         try:
-            parser = self.customs.get(ctx.guild.id)[index - 1]
+            parser = list(self.customs.get(ctx.guild.id))[index - 1]
         except IndexError:
             await ctx.send(f"Invalid number check numbers using `{ctx.clean_prefix}mcparser custom list`.")
             return
