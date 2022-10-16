7212763efe32a94b9d9d4810b91cb6e5fdd04890
dia ♡ <60399731+6days9weeks@users.noreply.github.com>
2022-10-16 20:50:49 +0600

    Update __init__.py

--- vanity_in_status/__init__.py
+++ vanity_in_status/__init__.py
@@ -37,6 +37,8 @@ class VanityInStatus(commands.Cog):
         data = await self.config.all_guilds()
         for x in data:
             guild = self.bot.get_guild(x)
+            if not guild:
+                continue
             if "VANITY_URL" in guild.features:
                 self.vanity_cache[guild.id] = await guild.vanity_invite()
         if not self.cached:
